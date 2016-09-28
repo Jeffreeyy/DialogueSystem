ffecf1568bdfac35c35593ac37edeac97029783e
Jeffreeyy <17844@ma-web.nl>
2016-09-28 10:27:18 +0200

    fixed ui

--- Assets/Scripts/DialogueContainer.cs
+++ Assets/Scripts/DialogueContainer.cs
@@ -11,21 +11,28 @@ public class DialogueContainer
     [XmlArrayItem("message")]
     public List<Dialogue> dialogues = new List<Dialogue>();
 
+    //Function to load the file
     public static DialogueContainer Load(TextAsset file)
     {
         TextAsset _xml = file;
 
+        //If there is no XML file, give an error
         if(_xml == null)
             Debug.LogError("No XML file found!");
 
+        //Create a serializer
         XmlSerializer serializer = new XmlSerializer(typeof(DialogueContainer));
 
+        //Create a reader
         StringReader reader = new StringReader(_xml.text);
 
+        //Deserialize the XML file and put them in the dialogues
         DialogueContainer dialogues = serializer.Deserialize(reader) as DialogueContainer;
 
+        //Close the file
         reader.Close();
 
+        //Return the dialogues
         return dialogues;
     }
 }
